6ab178f9b3d5892e71ee6550eee5284d2d093e4d
sobhana1 <sobhana7boxi@gmail.com>
2017-03-09 13:15:52 +0530

    newly added

--- BinarySearch.java
+++ BinarySearch.java
@@ -5,38 +5,19 @@ public class BinarySearch {
 	
 
 	public static void main(String[] args) {
-		int n,array[],c,first,last,middle,search;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter number of Elements");
-		n=sc.nextInt();
-		array=new int[n];
-		
-		System.out.println("Enter"+n+"numbers");
-		
-		for(c=0;c<n;c++){
-			array[c]=sc.nextInt();
-		}
-		
-		System.out.println("Enter a number to search");
-		search=sc.nextInt();
-		first=0;
-		last=n-1;
-		middle=(first+last)/2;
-		
-		while(first <last){
-			if(array[middle]< search){
-				first=middle +1;
-			}else if(array[middle] == search){
-				System.out.println("Found");
-			}else last=middle - 1;
-			
-			middle=(first+last)/2;
-		}
-		
-		if(first > last){
-			System.out.println("Not found");
-		}
-		
+	int r,sum = 0,temp;
+	int n=454;
+	
+	temp=n;
+	while(n>0){
+	r=n%10;
+	sum=(sum*10)+r;
+	n=n/10;
 	}
-
+	
+	if(temp==sum){
+		System.out.println("is a pallindrome");
+	}else System.out.println("not a pallindrome");
+	}
+		
 }
